bb7168c32cc448317d7e0fda6e5e51e635f2eb05
CecilioN7 <111897620+CecilioN7@users.noreply.github.com>
2024-02-29 18:25:28 -0800

    Add files via upload

--- app/src/main/java/com/example/motif_application/ui/transpose/placeholder.java
+++ app/src/main/java/com/example/motif_application/ui/transpose/placeholder.java
@@ -1,4 +1,16 @@
 package com.example.motif_application.ui.transpose;
 
+import androidx.lifecycle.MutableLiveData;
+
 public class placeholder {
+    private final MutableLiveData<String> mText;
+
+    public placeholder() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Main menu page");
+    }
+
+    public placeholder(MutableLiveData<String> mText) {
+        this.mText = mText;
+    }
 }
